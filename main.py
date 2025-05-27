cb044ee12e65add426400793006b2d7d833945f1
Vikram <140962452+vikranth007@users.noreply.github.com>
2025-05-27 14:49:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,24 @@
-import joblib
 from fastapi import FastAPI
-from pydantic   import BaseModel
-
-
-model = joblib.load("genre_model.pkl")
-vectorizer = joblib.load("tfidf_vectorizer.pkl")
-
-class MovieInput(BaseModel):
-    description : str
-
+from pydantic import BaseModel
+import pickle
 
 app = FastAPI()
 
+# Load vectorizer and model
+with open("Tfidf_vectorizer.pkl", "rb") as f:
+    vectorizer = pickle.load(f)
+with open("genre_model.pkl", "rb") as f:
+    model = pickle.load(f)
+
+class InputText(BaseModel):
+    description: str
+
+@app.get("/")
+def read_root():
+    return {"message": "Movie Genre Classifier is running."}
 
 @app.post("/predict")
-
-def predict_genre(data: MovieInput):
-    text = [data.description.lower()]
-    text_vec = vectorizer.transform(text)
-    prediction = model.predict(text_vec)[0]
-    print(f"Input: {text[0]}")
-    print(f"Prediction: {prediction}")
-    return {"genre": prediction}
-
-
+def predict_genre(input: InputText):
+    text_vector = vectorizer.transform([input.description])
+    prediction = model.predict(text_vector)
+    return {"genre": prediction[0]}
